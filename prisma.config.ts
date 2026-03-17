bafce2cf076b4610a1c9c132e4d98919274338f1
Mustafiz Munna <developermunna360@gmail.com>
2026-03-17 20:44:29 +0600

    Attempt to fix build issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from "prisma/config";
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  earlyAccess: true,
+  environments: {
+    dev: { url: process.env.DATABASE_URL },
+  },
 });
